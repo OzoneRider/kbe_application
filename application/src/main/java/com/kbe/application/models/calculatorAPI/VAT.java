b84701f2bc519ddfe79a8a5ae5a7ac5d02d8e663
JoJo <Georgios.Padopoulos@outlook.de>
2022-03-16 20:09:47 +0100

    added calculator model

---
+++ application/src/main/java/com/kbe/application/models/calculatorAPI/VAT.java
@@ -0,0 +1,15 @@
+package com.kbe.application.models.calculatorAPI;
+
+public class VAT {
+    private double vatPrice;
+
+    public VAT(){}
+
+    public double getVatPrice() {
+        return vatPrice;
+    }
+
+    public void setVatPrice(double vatPrice) {
+        this.vatPrice = vatPrice;
+    }
+}
