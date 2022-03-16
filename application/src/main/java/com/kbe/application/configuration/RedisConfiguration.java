ed8a197b18add742ea38a3f5d01c5af48d2787bc
JoJo <Georgios.Padopoulos@outlook.de>
2022-03-16 19:52:03 +0100

    updated redis configuration

--- application/src/main/java/com/kbe/application/configuration/RedisConfiguration.java
+++ application/src/main/java/com/kbe/application/configuration/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.kbe.application.configuration;
 
+import com.kbe.application.models.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,5 +9,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
 
+    @Bean
+    public RedisTemplate<String, Product> redisTemplate(){
+        RedisTemplate<String, Product> productTemplate = new RedisTemplate<>();
+        productTemplate.setConnectionFactory(redisConnectionFactory());
+        return productTemplate;
+    }
 }
