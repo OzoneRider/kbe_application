a8a176e1677d079feeacd9181c1ce4883211d711
JoJo <Georgios.Padopoulos@outlook.de>
2022-03-19 22:15:07 +0100

    implemented storage api service

--- application/src/main/java/com/kbe/application/services/storageAPI/StorageService.java
+++ application/src/main/java/com/kbe/application/services/storageAPI/StorageService.java
@@ -1,6 +1,10 @@
 package com.kbe.application.services.storageAPI;
 
+import com.kbe.application.models.storageAPI.DeliveryInformation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,5 +14,18 @@ public class StorageService {
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String URL = "http://kbe-storage";
+    private final String URL = "http://kbe-storage/DeliveryInformation";
+
+    public DeliveryInformation importDeliveryInformation(int productId){
+        return restTemplate.getForObject(URL+"/"+productId, DeliveryInformation.class);
+    }
+
+    public DeliveryInformation exportDeliveryInformation(DeliveryInformation deliveryInformation){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        HttpEntity<DeliveryInformation> request = new HttpEntity<DeliveryInformation>(deliveryInformation, headers);
+        return restTemplate.postForObject(URL, request, DeliveryInformation.class);
+    }
+
+
 }
